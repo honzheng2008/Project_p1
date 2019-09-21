da653f3d6e4177643fe400bea26008d2b4e6a705
honzheng2008 <honzheng2008@gmail.com>
2019-09-21 15:34:50 -0400

    Amicablenumbers

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -10,18 +10,57 @@ namespace ConsoleApplication6
     {
         static void Main(string[] args)
         {
-           
-            
+
+            //  Amicablenumbers 
+            //int j = Amicablenumbers(10000); //31626  20190921
+
 
 
         }
+        /*
+         Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
 
-        void Amicablenumbers()
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+             // 31626
+             */
+        static int Amicablenumbers(int max)
         {
+            int sum = 0;
 
+            for(int i = 1; i < max; i++)
+            {
+                int j = sumOfDivisor(i);
+
+                if (j != i && sumOfDivisor(j) == i)
+                {
+                    sum += i;  //
+                }
+            }
+              
+            return sum ;
+        }
+
+       static  int sumOfDivisor(int n)
+        {
+            int sum = 0;
+            for(int i =1; i < n; i++)
+            {
+
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
 
 
         }
+
+
 
         void Capitals_Countries_Question()
         {
