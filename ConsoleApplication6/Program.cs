c3bf48f56fdf1a9779f3d1d033025e911099bf62
honzheng2008 <honzheng2008@gmail.com>
2019-09-21 15:15:56 -0400

    first commit

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -10,6 +10,67 @@ namespace ConsoleApplication6
     {
         static void Main(string[] args)
         {
+           
+            
+
+
+        }
+
+        void Amicablenumbers()
+        {
+
+
+
+        }
+
+        void Capitals_Countries_Question()
+        {
+            Dictionary<string, string> Data = new Dictionary<string, string>();
+            string input = "";
+            string countryName = "";
+            string countryCapital = "";
+            Data.Add("CANADA", "OTTAWA");
+            Data.Add("CHINA", "BEIJING");
+            Data.Add("ECUADOR", "QUITO");
+            Data.Add("ERITREA", "ASMARA");
+            Data.Add("FIJI", "SUVA");
+
+            Dictionary<string, string> temp = Data;
+            int number = Data.Count;
+
+
+            while (true)
+            {
+                number = temp.Count;
+                Random random = new Random();
+                int randomNumber = random.Next(0, number);
+                countryName = temp.ElementAt(randomNumber).Key;
+                countryCapital = temp.ElementAt(randomNumber).Value;
+
+                Console.WriteLine("What is capital for " + countryName);
+                input = Console.ReadLine();
+
+                if (countryCapital.Equals(input))
+                {
+                    temp.Remove(countryName);
+                    Console.WriteLine("Right!");
+
+                    if (temp.Count == 0)
+                    {
+                        break;
+                    }
+
+                }
+
+
+
+            }
+
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+
+
         }
     }
 }
